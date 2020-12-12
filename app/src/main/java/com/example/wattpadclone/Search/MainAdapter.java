58a2e64001204241537cc0646c3c69d10d4b663f
vanphuoc69 <71546114+vanphuoc69@users.noreply.github.com>
2020-12-13 00:31:32 +0700

    vp

--- app/src/main/java/com/example/wattpadclone/Search/MainAdapter.java
+++ app/src/main/java/com/example/wattpadclone/Search/MainAdapter.java
@@ -1,4 +1,4 @@
-package com.example.wattpadclone;
+package com.example.wattpadclone.Search;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.wattpadclone.R;
 
 public class MainAdapter extends BaseAdapter {
     private Context context;
