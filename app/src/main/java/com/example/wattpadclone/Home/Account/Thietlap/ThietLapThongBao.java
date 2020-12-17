67ac954858021f633edc6a88a9a591768993a6e6
vanphuoc69 <71546114+vanphuoc69@users.noreply.github.com>
2020-12-17 16:24:23 +0700

    vp

--- app/src/main/java/com/example/wattpadclone/Home/Account/Thietlap/ThietLapThongBao.java
+++ app/src/main/java/com/example/wattpadclone/Home/Account/Thietlap/ThietLapThongBao.java
@@ -4,23 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
-import com.example.wattpadclone.Home.Account.AccountActivity;
 import com.example.wattpadclone.R;
 
-public class thiet_lap_chung extends AppCompatActivity {
-    TextView txtThietLapTK;
+public class ThietLapThongBao extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_thiet_lap_chung);
-
-        Toolbar toolbar = findViewById(R.id.toolbar_thiet_lap_chung);
+        setContentView(R.layout.activity_thiet_lap_thong_bao);
+        Toolbar toolbar = findViewById(R.id.toolbar_thiet_lap_tb);
         toolbar.setLogo(ContextCompat.getDrawable(this, R.drawable.ic_baseline_arrow_back_24));
         View logoView = toolbar.getChildAt(1);
         logoView.setOnClickListener(new View.OnClickListener() {
@@ -29,15 +24,5 @@ public class thiet_lap_chung extends AppCompatActivity {
                 onBackPressed();
             }
         });
-
-        txtThietLapTK = findViewById(R.id.txtThietLapTK);
-        txtThietLapTK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(thiet_lap_chung.this, ThietLapTaiKhoan.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
